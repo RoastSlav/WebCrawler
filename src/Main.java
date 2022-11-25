bad205f9ebd2445fe27eb68dfef1d189f2d2bdd9
RoastSlav <raikov0411@gmail.com>
2022-11-25 12:29:32 +0200

    Made the processing of pages multithreaded

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,54 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
+    static class ProcessableLink implements Runnable {
+        HttpClient httpClient = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).build();
+        String link;
+
+        ProcessableLink(String link) {
+            this.link = link;
+            PROCESSED_LINKS.add(link);
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Currently processing page: " + link);
+            HttpRequest request = buildRequest(link);
+            try {
+                HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                Document document = Jsoup.parse(response.body(), response.uri().toString());
+                String[] followLinks = getFollowLinks(document, link);
+                for (String followLink : followLinks) {
+                    if (!PROCESSED_LINKS.contains(followLink))
+                        threadPool.submit(new ProcessableLink(followLink));
+                }
+                getImagesFromDoc(document, httpClient);
+            } catch (Exception e) {
+                System.out.println("Problem processing page: " + link);
+            }
+        }
+    }
+
     static final String PROGRAM_NAME = "WebCrawler";
-    private static final Set<String> PROCESSED_IMAGES = new HashSet<>();
-    private static final Set<String> PROCESSED_LINKS = new HashSet<>();
-    private static final Queue<String> LINKS_TO_PROCESS = new LinkedList<>();
+    private static final Set<String> PROCESSED_IMAGES = Collections.synchronizedSet(new HashSet<>());
+    private static final Set<String> PROCESSED_LINKS = Collections.synchronizedSet(new HashSet<>());
+    private static final Set<String> FORMATS_TO_DOWNLOAD = Collections.synchronizedSet(new HashSet<>());
     private static final HelpFormatter FORMATTER = new HelpFormatter();
-    private static final Set<String> FORMATS_TO_DOWNLOAD = new HashSet<>();
-    private static final HttpClient httpClient = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).build();
+    private static final int DEFAULT_THREAD_COUNT = 5;
+    private static ExecutorService threadPool;
     private static CommandLine cmd = null;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Options options = intializeOptions();
         CommandLineParser cmdParser = new DefaultParser();
         try {
@@ -41,13 +70,16 @@ public class Main {
             return;
         }
 
-        String[] baseUrl = cmd.getOptionValues("url");
-        LINKS_TO_PROCESS.addAll(List.of(baseUrl));
-        for (String url : baseUrl)
-            processLinks(url);
+        threadPool = Executors.newFixedThreadPool(DEFAULT_THREAD_COUNT);
 
+        String[] baseUrls = cmd.getOptionValues("url");
         String[] formats = cmd.getOptionValues("iFormat");
-        FORMATS_TO_DOWNLOAD.addAll(List.of(formats));
+        if (formats != null)
+            FORMATS_TO_DOWNLOAD.addAll(List.of(formats));
+
+        for (String baseUrl : baseUrls) {
+            threadPool.submit(new ProcessableLink(baseUrl));
+        }
     }
 
     private static Options intializeOptions() {
@@ -68,32 +100,12 @@ public class Main {
 
         String userAgent = cmd.getOptionValue("uAgent");
         if (userAgent != null)
-            requestBuilder.header("User-Agent", userAgent);
+            requestBuilder.setHeader("User-Agent", userAgent);
 
         return requestBuilder.build();
     }
 
-    private static void processLinks(String baseUrl) throws Exception {
-        while (!LINKS_TO_PROCESS.isEmpty()) {
-            String link = LINKS_TO_PROCESS.remove();
-            if (PROCESSED_LINKS.contains(link))
-                continue;
-
-            System.out.println("Currently processing page: " + link);
-            HttpRequest request = buildRequest(link);
-            Document document = getDocument(request);
-            getImagesFromDoc(document);
-            PROCESSED_LINKS.add(link);
-            LINKS_TO_PROCESS.addAll(List.of(getFollowLinks(document, baseUrl)));
-        }
-    }
-
-    private static Document getDocument(HttpRequest request) throws Exception {
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        return Jsoup.parse(response.body(), response.uri().toString());
-    }
-
-    private static void getImagesFromDoc(Document doc) throws Exception {
+    private static void getImagesFromDoc(Document doc, HttpClient client) throws Exception {
         Elements images = doc.getElementsByTag("img");
         String saveDirectory = cmd.getOptionValue("oDir");
         if (saveDirectory == null)
@@ -105,13 +117,13 @@ public class Main {
                 continue;
             PROCESSED_IMAGES.add(src);
 
-            downloadImage(src, saveDirectory);
+            downloadImage(src, saveDirectory, client);
         }
     }
 
-    private static void downloadImage(String src, String saveDirectory) throws IOException, InterruptedException {
+    private static void downloadImage(String src, String saveDirectory, HttpClient client) throws IOException, InterruptedException {
         HttpRequest request = buildRequest(src);
-        HttpResponse<byte[]> response = httpClient.send(request, HttpResponse.BodyHandlers.ofByteArray());
+        HttpResponse<byte[]> response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
 
         Optional<String> contentTypeOptional = response.headers().firstValue("content-type");
         String fileName = getFileName(src, contentTypeOptional);
@@ -143,7 +155,7 @@ public class Main {
         int fileNameStartIndex = src.lastIndexOf('/') + 1;
         int fileNameEndIndex = src.lastIndexOf('.');
         String fileName = src.substring(fileNameStartIndex, fileNameEndIndex);
-        if (fileExtension != null && !FORMATS_TO_DOWNLOAD.contains(fileExtension))
+        if (fileExtension != null && FORMATS_TO_DOWNLOAD.contains(fileExtension))
             return null;
 
         return fileName + "." + fileExtension;
@@ -154,14 +166,7 @@ public class Main {
         Elements links = doc.getElementsByTag("a");
         for (Element link : links) {
             String href = link.attr("abs:href");
-            URL linkUrl;
-            try {
-                linkUrl = new URL(href);
-            } catch (Exception e) {
-                continue;
-            }
-            String linkUrlHost = linkUrl.toExternalForm();
-            if (linkUrlHost.contains(pageUrl))
+            if (href.contains(pageUrl))
                 followLinks.add(href);
         }
         return followLinks.toArray(String[]::new);
